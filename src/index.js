0a04cc84b98ad9d88bf79fa2047e21d7ca7aea4f
Anastasia Savrukhina <assavr0@gmail.com>
2023-10-29 23:47:15 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    const stack = [];
+    for (const bracket of str) {
+      const pair = bracketsConfig.find((pair) => pair.includes(bracket));
+      if (bracket === pair[1] && stack.length === 0 && pair[0] !== pair[1]) {
+        return false;
+      }  else if (bracket === pair[1] && stack.at(-1) === pair[0]) {
+        stack.pop();
+      } else if (bracket === pair[0]) {
+        stack.push(bracket);
+      } else if (bracket === pair[1] && stack.at(-1) !== pair[0]) {
+        return false;
+      }
+    }
+    return stack.length === 0;
+  }
